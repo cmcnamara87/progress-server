4b4a8477b6b131fb84e82c4ffe600c678334aac0
Craig McNamara <cmcnamara87@gmail.cok>
2014-06-22 14:42:58 +1000

    Change return type to be JSON

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 require 'vendor/autoload.php';
 require 'rb.phar';
-
+// header('Content-type: application/json');
 
 $app = new \Slim\Slim(array(
     'debug' => true
 ));
+$app->contentType("application/json");
 
 // require 'push/push.php';
 
@@ -22,12 +23,12 @@ require 'routes/public.php';
 require 'db/db.php';
 
 // Add session middleware
-$app->add(new \Slim\Middleware\SessionCookie(
-	array(
-		'secret' => 'thisismysecret',
-		'expires' => '7 days',
-	))
-);
+// $app->add(new \Slim\Middleware\SessionCookie(
+// 	array(
+// 		'secret' => 'thisismysecret',
+// 		'expires' => '7 days',
+// 	))
+// );
 // Add camelcase middleware
 $app->add(new \CamelCaseMiddleware());
 
