cb27bdb8e117c5a80c41c6216df8378ef4431cdb
Craig McNamara <cmcnamara87@gmail.cok>
2014-06-23 17:46:17 +1000

    Add public endpoint to see peoples progress

--- routes/public.php
+++ routes/public.php
@@ -20,3 +20,35 @@ $app->get('/setup', function() {
 	R::store($user);
 
 });
+
+$app->get('/users/:userId/projects', function($userId) {
+	$user = R::load('user', $userId);
+	$projects = $user->ownProjectList;
+	foreach($projects as $project) {
+		$time = 0;
+		$previousTime = null;
+		foreach($project->ownProgressList as $progress) {
+			$fiveMinAgo = $progress->created - (5 * 60);
+			if($previousTime && ($progress->created - $previousTime) < (5 * 60)) {
+				continue;
+			}
+			$previousTime = $progress->created;
+			$time += 5;
+		}
+		$project->time = $time;
+		
+
+		// echo json_encode($project->export(), JSON_NUMERIC_CHECK);
+		// die();
+	}
+	$export = array_map(function($project) {
+		$result = new stdClass();
+		$result->id = $project->id;
+		$result->name = $project->name;
+		$result->time = $project->time;
+		$result->directories = R::exportAll($project->ownDirectoryList);
+		return $result;
+	}, $projects);
+
+	echo json_encode(array_values($export), JSON_NUMERIC_CHECK);
+});
