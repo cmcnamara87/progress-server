e56427c8554f3db9faf6db5fbc78750442d17475
Craig McNamara <cmcnamara87@gmail.cok>
2014-12-03 21:18:34 +1000

    refactor(registration): make mass assignable

--- src/app/models/User.php
+++ src/app/models/User.php
@@ -16,6 +16,8 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	protected $table = 'users';
 
+	protected $fillable = ['name','username','email','password','picture'];
+
 	/**
 	 * The attributes excluded from the model's JSON form.
 	 *
